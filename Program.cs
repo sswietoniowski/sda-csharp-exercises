9b36bae5c8f4b73823b34f205f948788f97de43d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-07 12:53:18 +0100

    Implemented basic version of function that would check password complexity

--- Program.cs
+++ Program.cs
@@ -6,6 +6,44 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(HasRequiredComplexity("abc"));
+            Console.WriteLine(HasRequiredComplexity("aA$bbb1"));
+        }
+
+        private static bool HasRequiredComplexity(string password)
+        {
+            bool hasRequiredLength = password.Length >= 6;
+            bool containsLowerCharacter = false;
+            bool containsUpperCharacter = false;
+            bool containsDigit = false;
+            string specialCharacters = "&*$#";
+            bool containsSpecialCharacter = false;
+            foreach (char character in password.ToCharArray())
+            {
+                if (Char.IsLower(character))
+                {
+                    containsLowerCharacter = true;
+                }
+
+                if (Char.IsLower(character))
+                {
+                    containsUpperCharacter = true;
+                }
+
+                if (Char.IsDigit(character))
+                {
+                    containsDigit = true;
+                }
+
+                if (specialCharacters.Contains(character))
+                {
+                    containsSpecialCharacter = true;
+                }
+            }
+
+            return hasRequiredLength && containsLowerCharacter &&
+                   containsUpperCharacter && containsDigit &&
+                   containsSpecialCharacter;
         }
     }
 }
