402d324da0b34061b6fe372248f865400d80069d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-20 11:26:59 +0100

    Added two constructor to the Person class

--- Person.cs
+++ Person.cs
@@ -26,6 +26,20 @@ namespace sda_csharp_exercises
 
         public int YearOfBirth { get; set; }
 
+        public Person() : this("", "", 1900)
+        {
+            Console.WriteLine("Called Person constructor without parameters.");
+        }
+
+        public Person(string firstName, string lastName, int yearOfBirth)
+        {
+            Console.WriteLine("Called Person constructor with parameters.");
+
+            FirstName = firstName;
+            LastName = lastName;
+            YearOfBirth = yearOfBirth;
+        }
+
         public void WhoAmI()
         {
             Console.WriteLine($"My name is {LastName}. {FirstName} {LastName}");
