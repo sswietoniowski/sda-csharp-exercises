76ccd7b09282a545ad22b7563cf2d2ddead9a9fa
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-20 11:07:47 +0100

    Implemented basic inheritance

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Person
+    {
+        private string firstName;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName { get; set; }
+
+        public int YearOfBirth { get; set; }
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"My name is {LastName}. {FirstName} {LastName}");
+        }
+
+        public bool IsAdult() => DateTime.Today.Year - YearOfBirth >= 18;
+        //{
+        //    return DateTime.Today.Year - YearOfBirth >= 18;
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,26 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
+            //Person person = new Person();
+            //person.WhoAmI();
+            //person.FirstName = "Jan";
+            //person.LastName = "Kowalski";
+            //person.YearOfBirth = 1981;
+            //Console.WriteLine(person.IsAdult());
+            //Person person = new Person()
+            //{
+            //    FirstName = "Jan",
+            //    LastName = "Kowalski",
+            //    YearOfBirth = 1981
+            //};
+            //person.WhoAmI();
+            //Console.WriteLine(person.IsAdult());
+            Employee employee = new Employee();
+            employee.FirstName = "Jan";
+            employee.LastName = "Kowalski";
+            employee.YearOfBirth = 1981;
+            employee.Salary = 5000;
+            employee.WhoAmI();
         }
     }
 }
