2259f2495c9b88cff0a0a724ba90c71e88c84800
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-20 12:03:49 +0100

    Type checking using GetType and typename

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sda_csharp_exercises
 {
@@ -6,14 +7,34 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee("Jan", "Kowalski", 1981, 5000);
-            Console.WriteLine(employee.Salary);
-            Person person = new Employee("Adam", "Nowak", 2000, 6000);
-            //Console.WriteLine(person.Salary);
-            Employee employee2 = (Employee)person;
-            Console.WriteLine(employee2.Salary);
-            Person person2 = new Person("Anna", "Kowalska", 1982);
-            Employee employee3 = (Employee)person2;
+            Person person = new Person("Jan", "Kowalski", 1981);
+            Employee employee = new Employee("Adam", "Nowak", 2000, 5000);
+            Person person2 = new Employee("Joanna", "Kowalska", 1982, 6000);
+            
+            List<Person> people = new List<Person>();
+            people.Add(person);
+            people.Add(employee);
+            people.Add(person2);
+
+            foreach (Person p in people)
+            {
+                if (p.GetType() == typeof(Person))
+                {
+                    p.WhoAmI();
+                    Console.WriteLine("Is Person");
+                } 
+                else if (p.GetType() == typeof(Employee))
+                {
+                    p.WhoAmI();
+                    Console.WriteLine("Is Employee");
+                    Employee e = (Employee)p;
+                    Console.WriteLine($"Salary = {e.Salary}");
+                }
+                else
+                {
+                    Console.WriteLine("No idea !?");
+                }
+            }
         }
     }
 }
