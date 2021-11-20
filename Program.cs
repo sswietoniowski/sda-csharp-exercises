5d458b459ac0ea442e2e8e93214f681af4902035
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-11-20 11:50:45 +0100

    Experiments with type casting

--- Program.cs
+++ Program.cs
@@ -6,31 +6,14 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            //Person person = new Person();
-            //person.WhoAmI();
-            //person.FirstName = "Jan";
-            //person.LastName = "Kowalski";
-            //person.YearOfBirth = 1981;
-            //Console.WriteLine(person.IsAdult());
-            //Person person = new Person()
-            //{
-            //    FirstName = "Jan",
-            //    LastName = "Kowalski",
-            //    YearOfBirth = 1981
-            //};
-            //person.WhoAmI();
-            //Console.WriteLine(person.IsAdult());
-            Employee employee = new Employee();
-            employee.FirstName = "Jan";
-            employee.LastName = "Kowalski";
-            employee.YearOfBirth = 1981;
-            employee.Salary = 5000;
-            employee.WhoAmI();
-            Employee employee2 = new Employee("Adam", "Nowak", 2000, 5000);
-            employee2.WhoAmI();
-            //Person person = new Person(firstName: "Adam", lastName: "Nowak");
-            //person.WhoAmI();
-            //Console.WriteLine(person.YearOfBirth);
+            Employee employee = new Employee("Jan", "Kowalski", 1981, 5000);
+            Console.WriteLine(employee.Salary);
+            Person person = new Employee("Adam", "Nowak", 2000, 6000);
+            //Console.WriteLine(person.Salary);
+            Employee employee2 = (Employee)person;
+            Console.WriteLine(employee2.Salary);
+            Person person2 = new Person("Anna", "Kowalska", 1982);
+            Employee employee3 = (Employee)person2;
         }
     }
 }
